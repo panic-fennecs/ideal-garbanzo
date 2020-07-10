7f6593cbd8deb13e7b11e5de831f3e7168f2ac2d
ichinii <sven.salt@yahoo.de>
2020-07-10 23:09:53 +0000

    move dog

--- Dog.gd
+++ Dog.gd
@@ -1,9 +1,26 @@
 extends Spatial
 
-
+var target_pos = null
+var speed = 8
 
 func _ready():
 	pass
 
 func _process(delta):
+	if target_pos:
+		var pos = transform.origin
+		var vec = target_pos - pos
+		var dir = vec.normalized()
+		var l = vec.length()
+		pos = pos + dir * speed * delta
+		transform.origin = pos
+		transform = transform.looking_at(target_pos, Vector3(0, 1, 0))
+		if l < 1:
+			target_pos = null
+
+func move_to(pos):
 	pass
+
+
+func _on_Ground_clicked(pos):
+	target_pos = pos
