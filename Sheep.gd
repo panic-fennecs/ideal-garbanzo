81536b3788ef576616a833266a075f558c962430
Bluemi <bruno.schilling@protonmail.ch>
2020-07-10 23:43:29 +0200

    sheep collision and movement

--- Sheep.gd
+++ Sheep.gd
@@ -1,14 +1,18 @@
-extends Spatial
+extends KinematicBody
 
 
-const DOG_REACTION_DISTANCE = 5
+const DOG_REACTION_DISTANCE = 7
+const MAX_VELOCITY = 10.0
+const DRAG = 0.05
 
-
-var speed = Vector3()
+var velocity = Vector2()
 var main
 
 func _ready():
 	main = get_node("/root/Main")
+
+func get_2d_position():
+	return Vector2(self.translation.x, self.translation.z)
 
 func get_other_sheep():
 	var other_sheep = []
@@ -23,5 +27,15 @@ func get_dog():
 func _physics_process(delta):
 	var dog = get_dog()
 	if dog.translation.distance_to(self.translation) < DOG_REACTION_DISTANCE:
-		print('near')
+		flee(Vector2(dog.translation.x, dog.translation.z))
 	#var other_sheep = get_other_sheep()
+	
+	velocity *= 1.0 - DRAG
+
+	move_and_collide(Vector3(velocity.x, 0, velocity.y) * delta)
+
+func flee(pos):
+	var diff = get_2d_position() - pos
+	var desired_velocity = diff.normalized() / (diff.length()+0.001) * MAX_VELOCITY 
+	var steering = desired_velocity - velocity
+	velocity += steering
