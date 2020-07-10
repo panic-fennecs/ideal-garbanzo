b4346bf0f2ae41e48c935dc698104b9d582c9ca6
Bluemi <bruno.schilling@protonmail.ch>
2020-07-10 22:44:41 +0200

    modified camera

--- Main.gd
+++ Main.gd
@@ -5,9 +5,9 @@ onready var SheepScene = preload("res://Sheep.tscn")
 var sheep = []
 
 func init_sheep():
-	for i in range(1):
+	for i in range(10):
 		var s = SheepScene.instance()
-		s.translate(Vector3(randf()*10.0, randf()*10.0, randf()*10.0))
+		s.translate(Vector3(randf()*10.0, 1, randf()*10))
 		# s.position = 
 		add_child(s)
 
