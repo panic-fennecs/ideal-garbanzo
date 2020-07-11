ed359c0e1100f2f0c61dc093bd7b2996d311ea6b
philipp <s0554213@htw-berlin.de>
2020-07-11 22:01:34 +0200

    fix Camera y offset

--- Camera.gd
+++ Camera.gd
@@ -1,9 +1,10 @@
 extends Camera
 
+const HEIGHT_OFFSET = 15
 var angle_offset: float = 15.0
 var camera_move_speed: float = 3.0
 
 func _physics_process(delta: float) -> void:
 	var dog_pos = $"/root/Main/Dog".transform.origin
-	var target_pos = Vector3(dog_pos.x, transform.origin.y, dog_pos.z + angle_offset)
+	var target_pos = Vector3(dog_pos.x, dog_pos.y + HEIGHT_OFFSET, dog_pos.z + angle_offset)
 	transform.origin = transform.origin + (target_pos - transform.origin) * delta * camera_move_speed
