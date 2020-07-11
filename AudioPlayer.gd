8ec7d461503b5a82415f0bf6546a2213c5ac1245
philipp <s0554213@htw-berlin.de>
2020-07-11 21:36:23 +0200

    worked on audio player

--- AudioPlayer.gd
+++ AudioPlayer.gd
@@ -1,16 +1,13 @@
-extends Node
+extends Spatial
 
 const MIN_VOLUME_LEVEL = -45
 const MAX_VOLUME_LEVEL = -12
 const FADE_SPEED = 5
 
 const BACKGROUND_STREAMS = [
-	# preload("res://res/audio/a_part.wav"),
-	#preload("res://res/audio/a_part_cut.wav"),1
-	#preload("res://res/audio/b_part.wav"),
-	#preload("res://res/audio/b_part_cut.wav"),
-	#preload("res://res/audio/d_part.wav"),
-	#preload("res://res/audio/d_part_cut.wav"),
+	preload("res://audio/SheepSoundtrackPart1.wav"),
+	preload("res://audio/SheepSoundtrackPart2.wav"),
+	preload("res://audio/SheepSoundtrackPart3.wav")
 ]
 
 # const WHIZ = preload("res://res/audio/whiz.wav")
@@ -20,18 +17,17 @@ var _fading_music_players = []
 var _sample_players = []
 
 var _current_level = 0
+var critical_level = 0
 
 class PlayerWrapper:
 	var player
-	var variation
 	var level
 	var volume
 	
-	func _init(level_arg, variation_arg=0, volume_arg=0):
+	func _init(level_arg, volume_arg=0):
 		self.level = level_arg
 		self.player = AudioStreamPlayer.new()
-		self.player.stream = BACKGROUND_STREAMS[self.level*2 + variation_arg]
-		self.variation = variation_arg
+		self.player.stream = BACKGROUND_STREAMS[self.level]
 		self.volume = volume_arg
 		self._update_volume_db()
 
@@ -46,7 +42,7 @@ class PlayerWrapper:
 		self.player.volume_db = pow(self.volume, 1.0/4.0) * (-MIN_VOLUME_LEVEL + MAX_VOLUME_LEVEL) + MIN_VOLUME_LEVEL
 
 	func refresh():
-		self.player.stream = BACKGROUND_STREAMS[self.level*2 + self.variation]
+		self.player.stream = BACKGROUND_STREAMS[self.level]
 		self.player.play()
 
 func play_sample(sample, volume_db=0.0):
@@ -61,21 +57,15 @@ func _ready():
 	$MusicRefreshTimer.connect("timeout", self, "_refresh_music")
 	$ChangePartTimer.connect("timeout", self, "_change_music")
 	$RestartTimer.connect("timeout", self, "_restart_music")
-	_current_music_player = PlayerWrapper.new(0, 0, 1)
+	_current_music_player = PlayerWrapper.new(0, 1)
 	add_child(_current_music_player.player)
 	_current_music_player.player.play()
 
 func _get_stream_index(level, variation):
 	return level*2 + variation
 
-func _get_critical_level():
-	var level = 0
-	if $"/root/Main/Level/Player0".health < 50:
-		level += 1
-	if $"/root/Main/Level/Player1".health < 50:
-		level += 1
-
-	return level
+func get_critical_level():
+	return critical_level
 
 func player_won():
 	_new_music(_current_level, 1)
@@ -94,7 +84,7 @@ func _refresh_music():
 		player.refresh()
 
 func _restart_music():
-	_new_music(0, 0, 1)
+	_new_music(0, 1)
 	$MusicRefreshTimer.start()
 	$ChangePartTimer.start()
 
@@ -103,19 +93,19 @@ func _stop_music():
 		_fading_music_players.append(_current_music_player)
 	_current_music_player = null
 
-func _new_music(level, variation=0, volume=0):
+func _new_music(level, volume=0):
 	var playing_position = 0
 	if _current_music_player != null:
 		_fading_music_players.append(_current_music_player)
 		playing_position = _current_music_player.player.get_playback_position()
 
-	_current_music_player = PlayerWrapper.new(level, variation, volume)
+	_current_music_player = PlayerWrapper.new(level, volume)
 	add_child(_current_music_player.player)
 	_current_music_player.play(playing_position)
 	_current_level = level
 
 func _change_music():
-	var level = _get_critical_level()
+	var level = get_critical_level()
 	if level != _current_level:
 		_new_music(level)
 
--- Main.gd
+++ Main.gd
@@ -11,9 +11,12 @@ func init_sheep():
 		add_child(s)
 		var end_pos = $"EndZone".global_transform.origin
 		end_pos = Vector2(end_pos.x, end_pos.z)
-		print(end_pos)
 		s.init(end_pos, end_pos + Vector2(0, 11))
 		sheep.append(s)
+
+func _input(event):
+	if event is InputEventKey and event.scancode == 65 and event.pressed:
+		AudioPlayer.critical_level = min(AudioPlayer.critical_level + 1, 2)
 
 func _ready():
 	init_sheep()
