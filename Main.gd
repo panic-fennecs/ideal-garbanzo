26304d9f95aa03a6d6cc717173135aa8ed0dd509
Bluemi <bruno.schilling@protonmail.ch>
2020-07-11 11:03:38 +0200

    reworked sheep movement

--- Main.gd
+++ Main.gd
@@ -5,9 +5,9 @@ onready var SheepScene = preload("res://Sheep.tscn")
 var sheep = []
 
 func init_sheep():
-	for i in range(50):
+	for i in range(20):
 		var s = SheepScene.instance()
-		s.translate(Vector3(randf()*10.0, 1, randf()*10))
+		s.translate(Vector3((randf()-0.5)*20.0, 1, (randf()-0.5)*20.0))
 		add_child(s)
 		sheep.append(s)
 
--- Sheep.gd
+++ Sheep.gd
@@ -1,17 +1,19 @@
 extends KinematicBody
 
 
-const DOG_REACTION_DISTANCE = 7
-const DOG_INFLUENCE = 2.2
-const OTHER_SHEEP_INFLUENCE = 0.8
+const DOG_REACTION_DISTANCE = 8
+const MAX_DOG_FORCE = 0.5
 
-const MAX_VELOCITY = 10.0
-const DRAG = 0.02
+const MAX_OTHER_SHEEP_FORCE = 0.5
+
+const MAX_VELOCITY = 8
+const DRAG = 0.05
 const EPSILON = 0.0001
 
-var velocity = Vector2()
 var main
 var move_animation = null
+var velocity = Vector2()
+# var panic = 0
 
 func _ready():
 	main = get_node("/root/Main")
@@ -31,18 +33,35 @@ func get_other_sheep():
 func get_dog():
 	return main.get_node("Dog")
 
-func _physics_process(delta):
-	# flee dog
+func flee_dog():
 	var dog = get_dog()
 	if dog.translation.distance_to(self.translation) < DOG_REACTION_DISTANCE:
-		flee(Vector2(dog.translation.x, dog.translation.z), DOG_INFLUENCE)
+		var pos = Vector2(dog.translation.x, dog.translation.z)
+		var diff = get_2d_position() - pos
+		var desired_velocity = diff * 100 / (diff.length() + 0.3)
+		var steering = desired_velocity - velocity
+		steering = steering.clamped(MAX_DOG_FORCE)
+		velocity = (velocity + steering).clamped(MAX_VELOCITY)
 
-	# follow other sheep
+func follow_other_sheep():
 	var other_sheep = get_other_sheep()
+
+	var sum_steering = Vector2()
+	var num_steerings = 0
 	for os in other_sheep:
-		var influence = OTHER_SHEEP_INFLUENCE / (os.translation.distance_squared_to(translation) + EPSILON)
-		var steering = os.velocity - velocity
-		velocity = velocity.linear_interpolate(velocity + steering, influence)
+		var diff = os.translation - translation
+		var desired_velocity = os.velocity.normalized() * MAX_VELOCITY
+		var steering = desired_velocity - velocity
+		var influence = 2.0 / (diff.length() + 1.0)
+		steering = steering.clamped(MAX_OTHER_SHEEP_FORCE) * influence
+		sum_steering += steering
+		num_steerings += 1
+	velocity = (velocity + sum_steering / num_steerings).clamped(MAX_VELOCITY)
+
+func _physics_process(delta):
+	flee_dog()
+
+	follow_other_sheep()
 
 	velocity *= 1.0 - DRAG
 
@@ -51,9 +70,3 @@ func _physics_process(delta):
 	else:
 		move_animation.move(Vector3(velocity.x, 0, velocity.y).normalized())
 		move_and_collide(Vector3(velocity.x, -translation.y, velocity.y) * delta)
-
-func flee(pos, influence):
-	var diff = get_2d_position() - pos
-	var desired_velocity = diff.normalized() / (diff.length()+EPSILON) * MAX_VELOCITY 
-	var steering = desired_velocity - velocity
-	velocity += steering * influence
