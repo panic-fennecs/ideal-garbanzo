aba8323b8e3d425af3706b717971ad8480d4d1b0
philipp <s0554213@htw-berlin.de>
2020-07-11 11:33:54 +0200

    dog model included

--- Dog.gd
+++ Dog.gd
@@ -1,12 +1,12 @@
 extends Spatial
 
 var target_pos = null
-var speed = 8
+var speed = 14
 var move_animation = null
 
 func _ready():
 	move_animation = $"MoveAnimation"
-	move_animation.set_objects($"MeshInstance", self)
+	move_animation.set_objects($"DogModel", self)
 
 func _process(delta):
 	if target_pos:
