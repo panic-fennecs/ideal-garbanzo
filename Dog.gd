b369046321eb8916d1154f1b61c3eff3edc70127
ichinii <sven.salt@yahoo.de>
2020-07-11 00:06:06 +0000

    dog smooth rotation

--- Dog.gd
+++ Dog.gd
@@ -6,7 +6,7 @@ var move_animation = null
 
 func _ready():
 	move_animation = $"MoveAnimation"
-	move_animation.set_object($"MeshInstance")
+	move_animation.set_objects($"MeshInstance", self)
 
 func _process(delta):
 	if target_pos:
@@ -16,15 +16,11 @@ func _process(delta):
 		var l = vec.length()
 		pos = pos + dir * speed * delta
 		transform.origin = pos
-		transform = transform.looking_at(target_pos, Vector3(0, 1, 0))
-		move_animation.move()
+		move_animation.move(dir)
 		if l < 1:
 			target_pos = null
 	else:
 		move_animation.idle()
-
-func move_to(pos):
-	pass
 
 func _on_Ground_clicked(pos):
 	target_pos = pos
--- MoveAnimation.gd
+++ MoveAnimation.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
-var object = null
-var moving = false
-var initial_height = 0
+var translated_object = null
+var rotated_object = null
 
+var moving = false
 var jumping = false
+var target_dir = null
+var initial_height = 0
 var height = 0
 var velocity = 0
 var force = 10
@@ -20,14 +22,21 @@ func _process(delta):
 	elif moving:
 		jumping = true
 		velocity = force
-	object.transform.origin.y = initial_height + height
+
+	translated_object.transform.origin.y = initial_height + height
+	if target_dir:
+		var dir = -rotated_object.global_transform.basis.z
+		dir = dir.linear_interpolate(target_dir, .02)
+		rotated_object.transform = rotated_object.transform.looking_at(rotated_object.transform.origin + dir, Vector3(0, 1, 0))
 
 func idle():
 	moving = false
 
-func move():
+func move(dir):
 	moving = true
+	target_dir = dir
 
-func set_object(o):
-	object = o
-	initial_height = object.transform.origin.y
+func set_objects(translated, rotated):
+	translated_object = translated
+	rotated_object = rotated
+	initial_height = translated_object.transform.origin.y
