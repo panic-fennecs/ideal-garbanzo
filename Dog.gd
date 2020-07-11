0f4c1c63ddbee28e0cdc72c1444db576916a86ae
ichinii <sven.salt@yahoo.de>
2020-07-11 19:29:24 +0000

    slide up quicker

--- Dog.gd
+++ Dog.gd
@@ -10,21 +10,31 @@ func _ready():
 	move_animation.force = move_animation.force * 2
 	move_animation.gravitation = move_animation.gravitation * 4
 
+func move(delta):
+	var pos = transform.origin
+	var vec = target_pos - pos
+	var dir = vec.normalized()
+	var horizontal_vec = Vector3(vec.x, 0, vec.z)
+	var horizontal_dir = horizontal_vec.normalized()
+	var horizontal_vel = horizontal_dir * speed
+	move_animation.move(horizontal_dir)
+	#transform.origin = Vector3(new_pos.x, pos.y, new_pos.z)
+	#move_and_slide(Vector3(0, -pos.y - 10, 0))
+	move_animation.move(horizontal_dir)
+	move_and_slide(Vector3(horizontal_vel.x, 0, horizontal_vel.z), Vector3(0, 1, 0))
+	var new_pos = transform.origin
+	vec = new_pos - pos
+	var l = vec.length()
+	l = max(0, horizontal_vel.length() * delta - l)
+	dir = vec.normalized()
+	var vel = dir * speed * l
+	move_and_slide(vel, Vector3(0, 1, 0))
+	move_and_collide(Vector3(0, -100, 0))
+	return horizontal_vec.length() < 1
+
 func _physics_process(delta):
 	if target_pos:
-		var pos = transform.origin
-		var vec = target_pos - pos
-		var dir = vec.normalized()
-		var horizontal_vec = Vector3(vec.x, 0, vec.z)
-		var horizontal_dir = horizontal_vec.normalized()
-		var horizontal_vel = horizontal_dir * speed
-		move_animation.move(horizontal_dir)
-		#transform.origin = Vector3(new_pos.x, pos.y, new_pos.z)
-		#move_and_slide(Vector3(0, -pos.y - 10, 0))
-		move_animation.move(horizontal_dir)
-		move_and_slide(Vector3(horizontal_vel.x, 0, horizontal_vel.z), Vector3(0, 1, 0))
-		move_and_collide(Vector3(0, -100, 0))
-		if horizontal_vec.length() < 1:
+		if move(delta):
 			target_pos = null
 	else:
 		move_animation.idle()
