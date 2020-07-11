2f42960b6d4fecf0461e581f5d2e465659f7f680
ichinii <sven.salt@yahoo.de>
2020-07-11 16:45:45 +0000

    move dog on mouse move while clicking

--- Ground.gd
+++ Ground.gd
@@ -1,7 +1,15 @@
 extends Spatial
 
 signal clicked(pos)
+var pressing = false
 
 func _on_StaticBody_input_event(camera, event, click_position, click_normal, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				pressing = true
+				emit_signal("clicked", click_position)
+			else:
+				pressing = false
+	if pressing and event is InputEventMouseMotion:
 		emit_signal("clicked", click_position)
