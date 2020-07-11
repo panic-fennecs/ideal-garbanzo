3035582b6ed076d863d2ed74acea118e6b047899
ichinii <sven.salt@yahoo.de>
2020-07-11 19:53:25 +0000

    add fence for end zone

--- Main.gd
+++ Main.gd
@@ -9,7 +9,10 @@ func init_sheep():
 		var s = SheepScene.instance()
 		s.translate(Vector3((randf()-0.5)*20.0, 1, (randf()-0.5)*20.0))
 		add_child(s)
-		s.init(Vector2(26.0, -34), Vector2(26, -23))
+		var end_pos = $"EndZone".global_transform.origin
+		end_pos = Vector2(end_pos.x, end_pos.z)
+		print(end_pos)
+		s.init(end_pos, end_pos + Vector2(0, 11))
 		sheep.append(s)
 
 func _ready():
