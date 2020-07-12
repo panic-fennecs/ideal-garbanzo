84778f28621b36bc9889a0c2cf89b670c198de2f
ichinii <sven.salt@yahoo.de>
2020-07-12 20:36:16 +0000

    level 1

--- Main.gd
+++ Main.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-onready var Level1 = preload("res://Level2.tscn")
+onready var Level1 = preload("res://Level1.tscn")
 onready var Level2 = preload("res://Level2.tscn")
 
 var sheep = []
