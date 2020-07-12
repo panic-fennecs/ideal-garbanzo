55f9ecd1fef4630ce22ea58f8bedc96bbc9e1d2e
ichinii <sven.salt@yahoo.de>
2020-07-12 14:29:37 +0000

    yellow hay; hay in level 1

--- Hay.gd
+++ Hay.gd
@@ -13,11 +13,11 @@ func mix(a, b, t):
 
 func _process(delta):
 	if dir:
-		transform = transform.looking_at(transform.origin + dir, Vector3.UP)
+		look_at(transform.origin + dir, Vector3.UP)
 		var vel = dir * mix(speed, 0, min(1, time))
 		var _c = move_and_slide(vel, Vector3.UP)
-		_c = move_and_collide(Vector3.UP * -100)
 		time = time + delta * damp
+	var _c = move_and_collide(Vector3.UP * -100)
 
 func push(dir2):
 	dir = Vector3(dir2.x, 0, dir2.y)
