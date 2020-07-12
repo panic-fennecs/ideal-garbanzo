24276e4128bee29435d618351e9c6b507430c43e
Philipp Bönsch <philipp.boensch@softgames.de>
2020-07-12 14:25:04 +0200

    achievement area defined

--- AchievementArea.gd
+++ AchievementArea.gd
@@ -1,8 +1,14 @@
 extends Area
 
+onready var Achievement = preload("res://Achievement.tscn")
+
+var entered_sheep = []
+
 func _on_AchievementArea_body_entered(body):
-	pass # Replace with function body.
-
-
-func _on_AchievementArea_body_exited(body):
-	pass # Replace with function body.
+	if body.is_in_group("sheep") and entered_sheep.find(body) != -1:
+		entered_sheep.append(body)
+		if entered_sheep.size() > 10:
+			var achievement = Achievement.instance()
+			$"/root/Main".add_child(achievement)
+			achievement.show_achievement("cross the river")
+			queue_free()
