c540c84ab51ce9797a7328c2481564b2238c0119
Philipp Bönsch <philipp.boensch@softgames.de>
2020-07-12 13:31:50 +0200

    end zone implemented

--- EndZone.gd
+++ EndZone.gd
@@ -1,4 +1,4 @@
 extends Spatial
 
 func _process(_delta):
-	$"Label3D/CanvasLayer/Label".text = $"/root/Main".get_num_finished_sheep() + "/" + String(len($"/root/Main".sheep)) + " sheep"
+	$"Label3D/CanvasLayer/Label".text = str($"/root/Main".get_num_finished_sheep()) + "/" + String(len($"/root/Main".sheep)) + " sheep"
--- Label3D.gd
+++ Label3D.gd
@@ -4,5 +4,10 @@ func _process(_delta):
 	var pos = global_transform.origin
 	var camera = $"/root/Main/Camera"
 	var offset_label =  $"CanvasLayer/Label".get_size() / 2
-	$"CanvasLayer/Label".set_position(camera.unproject_position(pos) - offset_label)
+	$"CanvasLayer/Label".set_position(camera.unproject_position(pos) + Vector2(0, 20) - offset_label)
 	$CanvasLayer/Sprite.set_position(camera.unproject_position(pos))
+	var dist = $"/root/Main/Dog".transform.origin.distance_to(global_transform.origin)
+	var alpha = 0.1
+	if dist / 200.0 < 1:
+		alpha = max(0.1, 1 - (dist / 200.0))
+	$"CanvasLayer/Label".modulate = Color(1, 1, 1, alpha)
