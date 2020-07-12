5f2f8273293aab8ae47081fa82e7e7defb657a98
Philipp Bönsch <philipp.boensch@softgames.de>
2020-07-12 19:29:18 +0200

    instruction achievement included

--- Level1.gd
+++ Level1.gd
@@ -1,8 +1,16 @@
 extends Spatial
 
+onready var Achievement = preload("res://Achievement.tscn")
+
 var hays = []
 
 func _ready():
+	var achievement = Achievement.instance()
+	$"/root/Main".add_child(achievement)
+	achievement.show_achievement("instructions", "lead all sheep into their new paddock", Color(105.0/255.0, 147.0/255.0, 202.0/255.0, 1.0))
+	
 	for c in get_children():
 		if c is KinematicBody and c.is_in_group("hay"):
 			hays.push_back(c)
+			
+	
