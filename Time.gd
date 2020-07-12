946a53e4a0008c1e9f684d98bd58f3c6596d9528
ichinii <sven.salt@yahoo.de>
2020-07-12 17:48:05 +0000

    add water to level 2

--- Time.gd
+++ Time.gd
@@ -9,5 +9,11 @@ func elapsed_time():
 	return OS.get_unix_time() - start_time
 
 func _process(delta):
-	$ElapsedTimeLabel.text = String(elapsed_time()) + " seconds"
+	var seconds = elapsed_time()
+	var minutes = floor(seconds / 60)
+	seconds = seconds % 60
+	if minutes > 0:
+		$ElapsedTimeLabel.text = String(minutes) + " minutes   " + String(seconds) + " seconds"
+	else:
+		$ElapsedTimeLabel.text = String(seconds) + " seconds"
 	$FpsLabel.text = String(Performance.get_monitor(Performance.TIME_FPS)) + " fps"
